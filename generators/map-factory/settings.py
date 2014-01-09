0865761b88712fd9ed83351dfee5626ca0af9471
Rotsee <leo_wallentin@hotmail.com>
2014-01-09 22:58:03 +0200

    added key names

--- generators/map-factory/settings.py
+++ generators/map-factory/settings.py
@@ -4,8 +4,10 @@ endDate   =  "2014-12-31"
 
 # wikidata-properties to get when generating the map
 # these can be sent to the client, for use in nation infoboxes (e.g. qtip)
-#there are only two types of properties: image and text
-imageProperties  = { 'flag_image': 'P41' }
-entityProperties = { 'capital': 'P36', 'currency' : 'P38', 'government' : 'P122' }
+# there are only two types of properties: image and text
+# NAME: (PROPERTYID, ABBREVIATION)
+#
+imageProperties  = { 'flag_image': ('P41', 'f') }
+entityProperties = { 'capital': ('P36', 'h'), 'currency' : ('P38', 'u'), 'government' : ('P122', 'g') }
 
 import login
